89473358efad8d46b29e9fdb7864ba240c075a3d
Shandy Odhiambo <56043500+ShandyBob@users.noreply.github.com>
2024-02-26 08:39:05 +0300

    Add files via upload

---
+++ Collections.py
@@ -0,0 +1,13 @@
+"""
+Lists,Tuples and Dictionaries
+"""
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
